b67df32c319bc18713a06b008449e30f49a11cf8
Arshdeep-13 <arshdeeprooprai@gmail.com>
2023-08-06 20:48:23 +0530

    Basic Layout added

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const shortUrl = require('./models/shortUrl');
+const app = express();
+const port = 3000;
+
+
+mongoose.connect('mongodb://localhost:27017', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', async (req, res) => {
+  const shortUrls = await shortUrl.find();
+  res.render('index', { shortUrls: shortUrls });
+});
+
+app.post('/shortUrls', async (req, res) => {
+  await shortUrl.create({ full: req.body.fullurl });
+  res.redirect('/');
+});
+
+app.get('/:shortUrl', async (req, res) => {
+  let url = await shortUrl.findOne({ short: req.params.shortUrl });
+  if (url == null) return res.sendStatus(404);
+
+  await url.clicks++; // Update the clicks property of the document
+  await url.save(); // Save the document back to the database
+
+  res.redirect(url.full); // Redirect to the full URL
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
